6a5745b5fecf111f955df49138266a7f2096ccec
Vanessa <vanessa.riosm571@gmail.com>
2023-08-05 15:40:40 -0500

    fix: remove adminHome component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import {Home} from './components/Home'
 import {ProductDetails} from './components/ProductDetails'
 import {ProductList} from './components/ProductList'
 import {Profile} from './components/Profile'
-import {AdminHome} from './components/admin/AdminHome'
+// import {AdminHome} from './components/admin/AdminHome'
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Route path='/productdetails' Component={ProductDetails} />
         <Route path='/productlist' Component={ProductList} />
         <Route path='/profile' Component={Profile} />
-        <Route path='/admin' Component={AdminHome} />
+        {/* <Route path='/admin' Component={AdminHome} /> */}
       </Routes>
     </div>
   )
