eb62d3ce10769784c6172d71a174a2d3d33fca96
Vanessa <vanessa.riosm571@gmail.com>
2023-08-14 15:41:59 -0500

    admin component was commented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import {ProductList} from './components/ProductList'
 import {Profile} from './components/Profile'
 import {Login} from './components/Login'
 import {Register} from './components/Register'
-import {AdminHome} from './components/admin/AdminHome'
+// import {AdminHome} from './components/admin/AdminHome'
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         <Route path='/profile' Component={Profile} />
         <Route path='/login' Component={Login} />
         <Route path='/Register' Component={Register} />
-        <Route path='/admin' Component={AdminHome} />
+        {/* <Route path='/admin' Component={AdminHome} /> */}
       </Routes>
     </div>
   )
