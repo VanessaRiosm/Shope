e9ebf0a39d3c7dfb9571402fa11b1c4f3a9ed158
Vanessa <vanessa.riosm571@gmail.com>
2023-08-30 15:10:49 -0500

    feat: add populate

--- api/src/controllers/users.controller.ts
+++ api/src/controllers/users.controller.ts
@@ -22,7 +22,7 @@ export const createUser = async (req: Request, res: Response) => {
 
 export const getUsers = async (req: Request, res: Response) => {
   try {
-    const users = await User.find()
+    const users = await User.find().populate('cart')
 
     res.json(users)
   } catch (err) {
