c680f95a00c3d69747e277f2f2fe28defeffbafd
Vanessa <vanessa.riosm571@gmail.com>
2023-09-08 11:58:51 -0500

    fix: admin design

--- client/src/components/AdminProducts.tsx
+++ client/src/components/AdminProducts.tsx
@@ -1,7 +1,17 @@
 import {useEffect} from 'react'
 import {useAppSelector, useAppDispatch} from '../hooks'
 import {fetchGetProducts} from '../features/products/productSlice'
-import {Box, Grid} from '@mui/material'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material'
+import {FaPencilAlt} from 'react-icons/fa'
+import {BsTrash3} from 'react-icons/bs'
 
 export const AdminProducts = () => {
   const products = useAppSelector((state: any) => state.product.productsList)
@@ -12,19 +22,78 @@ export const AdminProducts = () => {
   }, [])
 
   return (
-    <Box>
+    // <Box
+    //   margin='30px'
+    //   bgcolor={{xs: 'green', sm: 'red', md: 'pink', lg: 'purple', xl: 'blue'}}
+    //   sx={{
+    //     display: 'grid',
+    //     gap: 1,
+    //     gridTemplateColumns: {
+    //       xs: 'repeat(1, 1fr)',
+    //       // sm: 'repeat(2, 1fr)',
+    //       // md: 'repeat(2, 1fr)',
+    //       lg: 'repeat(2, 1fr)',
+    //     },
+    //   }}
+    // >
+    //   {products &&
+    //     products.map((product: any) => (
+    //       <Grid key={product.id} xs={1}>
+    //         <Box display='flex' flexDirection='column' alignItems='center'>
+    //           <img style={{maxWidth: 500, height: 500}} src={product.image} />
+
+    //           <Box>{product.name}</Box>
+    //           <Box>{product.price}</Box>
+    //           <Box>{product.category}</Box>
+    //         </Box>
+    //       </Grid>
+    //     ))}
+    // </Box>
+
+    <Box
+      margin='30px'
+      sx={{
+        display: 'grid',
+        gap: 1,
+        gridTemplateColumns: {
+          xs: 'repeat(1, 1fr)',
+          sm: 'repeat(2, 1fr)',
+          md: 'repeat(3, 1fr)',
+          lg: 'repeat(4, 1fr)',
+        },
+      }}
+    >
       {products &&
         products.map((product: any) => (
-          <Grid key={product.id} xs={1}>
-            <Box display='flex' flexDirection='column' alignItems='center'>
-              <img style={{maxWidth: 500, height: 500}} src={product.image} />
-
-              <Box>{product.name}</Box>
-              <Box>{product.price}</Box>
-              <Box>{product.category}</Box>
-              <Box>{product.description}</Box>
-            </Box>
-          </Grid>
+          <Box>
+            <Card sx={{maxWidth: 320}}>
+              <CardMedia
+                component='img'
+                alt='green iguana'
+                height='160'
+                image={product.image}
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='div'>
+                  {product.name}
+                </Typography>
+                <Typography variant='body1' color='black'>
+                  price: ${product.price}
+                </Typography>
+                <Typography variant='body1' color='black'>
+                  category: {product.category}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size='medium'>
+                  <FaPencilAlt />
+                </Button>
+                <Button size='medium'>
+                  <BsTrash3 />
+                </Button>
+              </CardActions>
+            </Card>
+          </Box>
         ))}
     </Box>
   )
--- client/src/components/AdminUsers.tsx
+++ client/src/components/AdminUsers.tsx
@@ -1,7 +1,17 @@
 import {useEffect} from 'react'
 import {useAppSelector, useAppDispatch} from '../hooks'
 import {fetchGetUsers} from '../features/users/userSlice'
-import {Box} from '@mui/material'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material'
+import {FaPencilAlt} from 'react-icons/fa'
+import {BsTrash3} from 'react-icons/bs'
 
 export const AdminUsers = () => {
   const users = useAppSelector((state: any) => state.user.usersList)
@@ -12,14 +22,50 @@ export const AdminUsers = () => {
   }, [])
 
   return (
-    <Box>
+    <Box
+      margin='30px'
+      sx={{
+        display: 'grid',
+        gap: 1,
+        gridTemplateColumns: {
+          xs: 'repeat(1, 1fr)',
+          sm: 'repeat(2, 1fr)',
+          md: 'repeat(3, 1fr)',
+          lg: 'repeat(4, 1fr)',
+        },
+      }}
+    >
       {users &&
         users.map((user: any) => (
-          <div>
-            <p>{user.email}</p>
-            <p>{user.username}</p>
-            <p>{user.rol}</p>
-          </div>
+          <Box>
+            <Card sx={{maxWidth: 320}}>
+              <CardMedia
+                component='img'
+                alt='green iguana'
+                height='140'
+                image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEpWWQrAJpIR6Xy7FhzhCT00vzSmT7xw9S2Q&usqp=CAU'
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='div'>
+                  {user.username}
+                </Typography>
+                <Typography variant='body1' color='black'>
+                  Email: {user.email}
+                </Typography>
+                <Typography variant='body1' color='black'>
+                  Role: {user.rol}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size='medium'>
+                  <FaPencilAlt />
+                </Button>
+                <Button size='medium'>
+                  <BsTrash3 />
+                </Button>
+              </CardActions>
+            </Card>
+          </Box>
         ))}
     </Box>
   )
