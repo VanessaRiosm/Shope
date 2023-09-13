8d6d864dbccb848beb2024d14e5ab2d8faaa6fe9
Vanessa <vanessa.riosm571@gmail.com>
2023-09-13 12:25:42 -0500

    feat: add no product found image

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -5,6 +5,7 @@ import {useAppDispatch, useAppSelector} from '../hooks'
 import {useEffect} from 'react'
 import {fetchSearchProducts} from '../features/products/productSlice'
 import {Link, useParams} from 'react-router-dom'
+import noproduct from '../images/noproduct.png'
 
 export const Search = () => {
   const products = useAppSelector((state: any) => state.product.productsFilter)
@@ -18,18 +19,6 @@ export const Search = () => {
   return (
     <Box>
       <NavBar />
-      {/* <Box
-        display='grid'
-        justifyItems='center'
-        marginTop='20px'
-        marginBottom='20px'
-      >
-        <Typography variant='h6' fontWeight='bold' mt='50px'>
-          Explore Shope's collection of knitwear that will give you a trendy and
-          sophisticated touch to your days. Our colors and details special items
-          will be the pop of color that your closet needs.
-        </Typography>
-      </Box> */}
 
       <Grid
         marginTop='80px'
@@ -38,7 +27,7 @@ export const Search = () => {
         columns={{xs: 1, sm: 2, md: 3, lg: 4}}
         justifyContent='center'
       >
-        {products &&
+        {products.length ? (
           products.map((product: Product) => (
             <Grid key={product.id} xs={1}>
               <Box display='flex' flexDirection='column' alignItems='center'>
@@ -56,7 +45,12 @@ export const Search = () => {
                 </Link>
               </Box>
             </Grid>
-          ))}
+          ))
+        ) : (
+          <Box>
+            <img src={noproduct} alt='No hay productos asi' height={'300rem'} />
+          </Box>
+        )}
       </Grid>
     </Box>
   )
