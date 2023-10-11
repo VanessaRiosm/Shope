79e47c4299ba4aaa8ee77da604f648f719bb1ac5
Vanessa <vanessa.riosm571@gmail.com>
2023-10-11 13:13:26 -0500

    fix: responsive search

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,4 +1,4 @@
-import {Box, Grid} from '@mui/material'
+import {Box, Typography} from '@mui/material'
 import {NavBar} from './NavBar'
 import {Product} from '../types/types'
 import {useAppDispatch, useAppSelector} from '../hooks'
@@ -20,38 +20,46 @@ export const Search = () => {
     <Box>
       <NavBar />
 
-      <Grid
-        marginTop='80px'
-        container
-        columnSpacing={0}
-        columns={{xs: 1, sm: 2, md: 3, lg: 4}}
-        justifyContent='center'
-      >
-        {products && products.length ? (
-          products.map((product: Product) => (
-            <Box sx={{display: {xs: 'grid', md: 'flex'}}} key={product.id}>
-              <Box display='flex' flexDirection='column' alignItems='center'>
-                <Link
-                  to={`/product/${product.id}`}
-                  style={{textDecoration: 'none', color: 'black'}}
+      {products && products.length ? (
+        <Box marginTop='80px'>
+          <Box
+            display={{xs: 'grid', md: 'flex'}}
+            justifyContent='center'
+            justifyItems='center'
+            flexWrap='wrap'
+            gap='30px'
+            maxWidth='200rem'
+          >
+            {products.map((product: Product) => (
+              <Box key={product.id}>
+                <Box
+                  display='flex'
+                  flexDirection='column'
+                  alignItems='center'
+                  textAlign='center'
                 >
-                  <img
-                    style={{maxWidth: 500, height: 500}}
-                    src={product.image}
-                  />
+                  <Link
+                    to={`/product/${product.id}`}
+                    style={{textDecoration: 'none', color: 'black'}}
+                  >
+                    <img
+                      style={{maxWidth: 500, height: 550}}
+                      src={product.image}
+                    />
 
-                  <Box>{product.name}</Box>
-                  <Box>{product.price}</Box>
-                </Link>
+                    <Typography>{product.name}</Typography>
+                    <Typography>${product.price}</Typography>
+                  </Link>
+                </Box>
               </Box>
-            </Box>
-          ))
-        ) : (
-          <Box mt='18rem'>
-            <img src={gif} alt='Loading' height={'70rem'} />
+            ))}
           </Box>
-        )}
-      </Grid>
+        </Box>
+      ) : (
+        <Box mt='18rem'>
+          <img src={gif} alt='Loading' height={'70rem'} />
+        </Box>
+      )}
     </Box>
   )
 }
